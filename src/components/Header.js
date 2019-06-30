ba0da232339ae8e34a1d061f4ec49752b6d6ac4a
Jesse <Jbell08@student.sl.on.ca>
2019-06-29 23:10:12 -0400

    started my interactive resume

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,24 +1,39 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import myface from '../images/myface.jpg'
 
 const Header = (props) => (
     <header id="header" style={props.timeout ? {display: 'none'} : {}}>
         <div className="logo">
-            <span className="icon fa-diamond"></span>
+        <span className="image main"><img src={myface} alt="" /></span>
         </div>
         <div className="content">
             <div className="inner">
-                <h1>Dimension</h1>
-                <p>A fully responsive site template designed by <a href="https://html5up.net">HTML5 UP</a> and released<br />
-                for free under the <a href="https://html5up.net/license">Creative Commons</a> license.</p>
+                <span><h1>Jesse Bell</h1><h2>Web Dev</h2></span>
+                <p>My Interactive Resume</p>
+                <p>Kingston, ON K7M 4W5 Canada</p>
+                <p>1(613)-449-4000</p>
+                <p>Jbell08@student.sl.on.ca</p>
+            </div>
+        </div>
+        <div className="content">
+            <div className="inner">
+                <h2>PROFILE</h2>
+                <p>Wrote my first HTML webpages when I was 11 years old and have been programming since. 
+                    I love programming and solving puzzles. Extremely creative, imaginatively driven. 
+                    I believe virtually nothing is impossible in this world if you just put your mind to it and maintain 
+                    a positive attitude. I recently completed the Computer Programming Analyst Course at St. 
+                    Lawrence College in Kingston Ontario. Which greatly accelerated my skills and in-depth understanding of 
+                    programming fundamentals. I also was a ISSC member and volunteer for two years. Excited to being part of a team,
+                     a career in what I love to do the most: learning and developing</p>
             </div>
         </div>
         <nav>
             <ul>
-                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('intro')}}>Intro</a></li>
-                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('work')}}>Work</a></li>
-                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('about')}}>About</a></li>
-                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('contact')}}>Contact</a></li>
+                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('experience')}}>Experience</a></li>
+                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('work')}}>Hard Skills</a></li>
+                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('about')}}>Soft Skills</a></li>
+                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('contact')}}>Reference</a></li>
             </ul>
         </nav>
     </header>
--- src/components/Main.js
+++ src/components/Main.js
@@ -4,6 +4,7 @@ import PropTypes from 'prop-types'
 import pic01 from '../images/pic01.jpg'
 import pic02 from '../images/pic02.jpg'
 import pic03 from '../images/pic03.jpg'
+import myface from '../images/myface.jpg'
 
 class Main extends React.Component {
   render() {
@@ -13,26 +14,46 @@ class Main extends React.Component {
     return (
       <div ref={this.props.setWrapperRef} id="main" style={this.props.timeout ? {display: 'flex'} : {display: 'none'}}>
 
-        <article id="intro" className={`${this.props.article === 'intro' ? 'active' : ''} ${this.props.articleTimeout ? 'timeout' : ''}`} style={{display:'none'}}>
-          <h2 className="major">Intro</h2>
-          <span className="image main"><img src={pic01} alt="" /></span>
-          <p>Aenean ornare velit lacus, ac varius enim ullamcorper eu. Proin aliquam facilisis ante interdum congue. Integer mollis, nisl amet convallis, porttitor magna ullamcorper, amet egestas mauris. Ut magna finibus nisi nec lacinia. Nam maximus erat id euismod egestas. By the way, check out my <a href="#work">awesome work</a>.</p>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis dapibus rutrum facilisis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Etiam tristique libero eu nibh porttitor fermentum. Nullam venenatis erat id vehicula viverra. Nunc ultrices eros ut ultricies condimentum. Mauris risus lacus, blandit sit amet venenatis non, bibendum vitae dolor. Nunc lorem mauris, fringilla in aliquam at, euismod in lectus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. In non lorem sit amet elit placerat maximus. Pellentesque aliquam maximus risus, vel sed vehicula.</p>
+        <article id="experience" className={`${this.props.article === 'experience' ? 'active' : ''} ${this.props.articleTimeout ? 'timeout' : ''}`} style={{display:'none'}}>
+          <h2 className="major">Recent Experience</h2>
+          <span className="image main"><img src={pic02} alt="" /></span>
+          <p><b>Lead web developer/QA tester/Consult</b> - GOFO, CPO1/CWO, Colonels and Captain (Navy) Professional and Self Development portals of the Defence Learning Network Learning Portals. Assumed responsibility of the monthly development of the General Officers and Flag Officers development. Replicated the system to meet over 300%- member increase.  Developed and tested over 60 webpages monthly with the content providers (Bruce Ewing and Dr. E). Consistently met with the clients to discuss future implementations and finely tuned attention to detail. Also, created instruction manuals on how to access the DLN LP via mobile device for members of the CAF and other LP users.</p>
+          <p><b>Mobile Web Developer</b> - Initialized the development of CAF Campus Mobile App with Ionic 4, Cordova 9 and Angular 7.</p>
+          <p><b>Mobile/Web Developer</b> - Transitions App - Assisted Marcy Steel (Lead) and Michael Poulin with preparing and inspecting content into a JSON file over 10000 lines long.</p>
+          <p><b>Enterprise Web Developer (C# .NET, LINQ) - eILP</b> - Worked with Michael Bennett (Lead). Addressing assigned tasks via Jira. Fixing Bugs, Testing and implementing small changes (production - hotfix).</p>
+          <p><b>Enterprise Web Developer (PHP)</b> - ISAT - Learned a lot from Matt Kasper (Supervisor) about complex enterprise web applications, how to debug and address tasks proper to workflow and programming standards, program file structure.</p>
+          <p><b>Courseware Developer</b> - Dangerous Goods Certification and Re-Certification Course - Used Saba/Lectora with templates from eLearning Brothers to assist with the development of an online course to train new comers and the current 15000 members </p>
+
           {close}
         </article>
 
         <article id="work" className={`${this.props.article === 'work' ? 'active' : ''} ${this.props.articleTimeout ? 'timeout' : ''}`} style={{display:'none'}}>
-          <h2 className="major">Work</h2>
+          <h2 className="major">Hard Skills</h2>
           <span className="image main"><img src={pic02} alt="" /></span>
-          <p>Adipiscing magna sed dolor elit. Praesent eleifend dignissim arcu, at eleifend sapien imperdiet ac. Aliquam erat volutpat. Praesent urna nisi, fringila lorem et vehicula lacinia quam. Integer sollicitudin mauris nec lorem luctus ultrices.</p>
-          <p>Nullam et orci eu lorem consequat tincidunt vivamus et sagittis libero. Mauris aliquet magna magna sed nunc rhoncus pharetra. Pellentesque condimentum sem. In efficitur ligula tate urna. Maecenas laoreet massa vel lacinia pellentesque lorem ipsum dolor. Nullam et orci eu lorem consequat tincidunt. Vivamus et sagittis libero. Mauris aliquet magna magna sed nunc rhoncus amet feugiat tempus.</p>
-          {close}
+          <p>HTML, CSS, SASS, JS, TypeScript, PHP, SQL, Angular 6/7, Ionic, XML, JSON, GSON, jQuery, AJAX</p>
+          <p>Implementation: Cordova, Apache, Tomcat, XAMMP, NodeJS, Express, VMware, Symfony, Doctrine, Composer</p>
+          <p>Workflow: Experience with Git and Gitflow methods for version control. Jira and PlanBox for task management and Confluence for central access of development documentations.</p> 
+          <p>Java, C#(ASP.NET, Razer/LINQ), C++</p>
+          <p>Photoshop, Lectora, Saba, eLearning - Courseware development.</p>
+          <p>Strong design (UI, UX, Manuals), program structure.</p>
+          <p>Quality assurance experience debugging, trouble shooting and problem-solving skills.</p>
+          <p>Mainframe: Z/OS, ISPF, COBOL, JCL, CICS</p>
+           {close}
         </article>
 
         <article id="about" className={`${this.props.article === 'about' ? 'active' : ''} ${this.props.articleTimeout ? 'timeout' : ''}`} style={{display:'none'}}>
-          <h2 className="major">About</h2>
+          <h2 className="major">Soft Skills</h2>
           <span className="image main"><img src={pic03} alt="" /></span>
-          <p>Lorem ipsum dolor sit amet, consectetur et adipiscing elit. Praesent eleifend dignissim arcu, at eleifend sapien imperdiet ac. Aliquam erat volutpat. Praesent urna nisi, fringila lorem et vehicula lacinia quam. Integer sollicitudin mauris nec lorem luctus ultrices. Aliquam libero et malesuada fames ac ante ipsum primis in faucibus. Cras viverra ligula sit amet ex mollis mattis lorem ipsum dolor sit amet.</p>
+          <p>Honest</p>
+          <p>Responsible</p>
+          <p>Efficient</p> 
+          <p>Productive</p>
+          <p>Enthusiastic</p>
+          <p>Friendly</p>
+          <p>Motivated</p>
+          <p>Leadership</p>
+          <p>Loyal</p> 
+          <p>Creative</p>
           {close}
         </article>
 
